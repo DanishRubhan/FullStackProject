44085607d98cc36c009caf2034640602938f0e7a
DanishRubhan <danishrubhan1610@gmail.com>
2024-05-10 20:09:39 +0530

    Modified

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ billingRoutes(app);
 surveyRoutes(app);
 
 if(process.env.NODE_ENV==='production') {
-
     app.use(express.static('client/build'));
-
     const path=require('path');
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
